fbe1f5391efcfe8f7bf9eb19d18abc4bb9b914d3
sachitha2 <shpsachitha@gmail.com>
2019-05-28 23:36:44 +0530

    Created file > SingleRowforShops.java

---
+++ app/src/main/java/com/example/icecreamdelivery/SingleRowForShops.java
@@ -0,0 +1,27 @@
+package com.example.icecreamdelivery;
+
+public class SingleRowForShops {
+    String name;
+    String age;
+
+    public SingleRowForShops(String name,String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public  String getAge(){
+        return  age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public  void  setAge(String age){
+        this.age = age;
+    }
+}
