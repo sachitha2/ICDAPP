3962f9d314ac4805b5d8ed6f46c6d53896cb5a79
sachitha2 <shpsachitha@gmail.com>
2019-05-29 00:08:55 +0530

    Adding onclick listner to shop list

--- app/src/main/java/com/example/icecreamdelivery/ShopsListAdapter.java
+++ app/src/main/java/com/example/icecreamdelivery/ShopsListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.icecreamdelivery;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -46,7 +48,33 @@ public class ShopsListAdapter extends BaseAdapter implements Filterable {
         textView.setText(originalArray.get(position).getName());
         textAge.setText(originalArray.get(position).getAge());
 
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intentSelectShop = new Intent(c, SelectShop.class);
+
+//                intentSelectShop.putExtra("ShopId", ShopId[position]);
+//                intentSelectShop.putExtra("ShopName", ShopName[position]);
+//                intentSelectShop.putExtra("Address", Address[position]);
+//                intentSelectShop.putExtra("Contact", Contact[position]);
+//                intentSelectShop.putExtra("Root", Root[position]);
+//                intentSelectShop.putExtra("IdNo", IdNo[position]);
+//                intentSelectShop.putExtra("Credit", Credit[position]);
+
+
+
+                intentSelectShop.putExtra("ShopId", 100);
+                intentSelectShop.putExtra("ShopName", "Sachitha Stores");
+                intentSelectShop.putExtra("Address", "No");
+                intentSelectShop.putExtra("Contact", "0715591137");
+                intentSelectShop.putExtra("Root","10");
+                intentSelectShop.putExtra("IdNo", "983142044v");
+                intentSelectShop.putExtra("Credit", "300");
+                c.startActivity(intentSelectShop);
+                Toast.makeText(c, "Shop NAme" + " was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return row;
     }
