6540046a4995d4943f03f2603846da65f7f6d95b
sachitha2 <shpsachitha@gmail.com>
2019-05-29 10:47:53 +0530

    Sales Item List Adapter class created

--- app/src/main/java/com/example/icecreamdelivery/SalesItem.java
+++ app/src/main/java/com/example/icecreamdelivery/SalesItem.java
@@ -2,14 +2,23 @@ package com.example.icecreamdelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SalesItem extends AppCompatActivity {
-
+    private ListView itemList;
+    EditText search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_item);
         setTitle("Sales - Items");
+
+        itemList = findViewById(R.id.salesItemList);
+        search = findViewById(R.id.searchSalesItems);
+
+
+
 
     }
 }
